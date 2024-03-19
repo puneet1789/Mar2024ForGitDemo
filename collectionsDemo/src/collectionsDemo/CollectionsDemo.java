d822710e8e72a702c44b910eb45cc2b19d0b9b9b
Puneet Gupta <puneetgupta1789@gmail.com>
2024-03-18 21:00:26 -0400

    Added my collections Java Class

---
+++ collectionsDemo/src/collectionsDemo/CollectionsDemo.java
@@ -0,0 +1,10 @@
+package collectionsDemo;
+
+public class CollectionsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
